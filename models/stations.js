bbaf6ab33847b3dd2c0d6aa63ab6a212b9e51273
Clara la Cour <denlacour@gmail.com>
2025-10-30 10:14:41 +0100

    migrations fix

--- models/stations.js
+++ models/stations.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       Stations.belongsTo(models.Companies, {
         foreignKey: 'companyId',
       });
+      // define association here
     }
   }
   Stations.init({
