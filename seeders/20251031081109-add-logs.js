da8edebc5c701c3828daa1dee98c550d276cbc6e
Melledodo <mkaagaard@hotmail.com>
2025-10-31 10:12:01 +0100

    logid update

--- seeders/20251031081109-add-logs.js
+++ seeders/20251031081109-add-logs.js
@@ -5,6 +5,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Logs", [
       {
+        id: 1,
         stationId: 1,
         comment: "Alt ser fint ud – ingen problemer med udstyr.",
         userId: 7,
@@ -12,6 +13,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 2,
         stationId: 1,
         comment: "Renset børster og fjernet snavs i afløb.",
         userId: 8,
@@ -19,6 +21,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 3,
         stationId: 2,
         comment: "En dyse var tilstoppet, men nu renset.",
         userId: 10,
@@ -26,6 +29,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 4,
         stationId: 2,
         comment: "Gulv spulet og vægge aftørret.",
         userId: 9,
@@ -33,6 +37,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 5,
         stationId: 3,
         comment: "Mindre oliepletter på gulvet – fjernet.",
         userId: 8,
@@ -40,6 +45,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 6,
         stationId: 3,
         comment: "Kontrolleret sensorer og filtre – alt OK.",
         userId: 7,
@@ -47,6 +53,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 7,
         stationId: 4,
         comment: "Facade rengjort og glaspartier pudset.",
         userId: 10,
@@ -54,6 +61,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 8,
         stationId: 5,
         comment: "Afløb udenfor vaskehallen renset.",
         userId: 9,
@@ -61,6 +69,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 9,
         stationId: 5,
         comment: "Alt udstyr kontrolleret – fungerer som det skal.",
         userId: 6,
@@ -68,6 +77,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        id: 10,
         stationId: 6,
         comment: "Omklædningsrum rengjort og desinficeret.",
         userId: 8,
