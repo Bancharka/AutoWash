b58c37096bd864088d72537590a94deed928ccbe
Clara la Cour <denlacour@gmail.com>
2025-10-31 10:26:13 +0100

    seed unit and products

--- seeders/20251031081047-add-units.js
+++ seeders/20251031081047-add-units.js
@@ -2,24 +2,36 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Units", [
+      {
+        id: 1,
+        name: "Liter",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        name: "Milliliter",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 3,
+        name: "Styk",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 4,
+        name: "Kilogram",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Units", null, {});
+  },
 };
--- seeders/20251031081059-add-products.js
+++ seeders/20251031081059-add-products.js
@@ -2,24 +2,66 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Products", [
+      {
+        name: "Alkalisk gulvrens",
+        unitId: 1, // Liter
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Skumrengøringsmiddel",
+        unitId: 2, // Milliliter
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Glasrens",
+        unitId: 1, // Liter
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Børste",
+        unitId: 3, // Styk
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Affedtningsmiddel",
+        unitId: 1, // Liter
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Microfiberklude",
+        unitId: 3, // Styk
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Gulvskrubbe",
+        unitId: 3, // Styk
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Sæbe til højtryksrenser",
+        unitId: 1, // Liter
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Kalkfjerner",
+        unitId: 1, // Liter
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Products", null, {});
+  },
 };
