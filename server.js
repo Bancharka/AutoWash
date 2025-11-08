833b49ad70b35e0e4f72b92fc0724b3c8298c425
Bancharka <olipet101@gmail.com>
2025-11-08 18:27:57 +0100

    footer, backarrow, logout
    Har fået lavet så footer navigationen ved hvilken url den er på, og agerer derefter. Har fået back arrow knappen til at gå til forrige side. Jeg har fået indsat icon til at logge ud, men funktionen hertil kommer senere.

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ const app = express();
 const PORT = 3000;
 
 app.engine(
-  "hbs",
-  engine({
-    extname: "hbs",
-    defaultLayout: "main",
-    layoutsDir: "./views/layouts",
-  })
+	"hbs",
+	engine({
+		extname: "hbs",
+		defaultLayout: "main",
+		layoutsDir: "./views/layouts",
+		helpers: {
+			eq: function (a, b) {
+				return a === b;
+			},
+		},
+	})
 );
 
 app.set("view engine", "hbs");
@@ -21,54 +26,68 @@ app.set("views", "./views/");
 // Bestemmer hvor vi henter static filer fra (f.eks CSS)
 app.use(express.static(path.join(__dirname, "public")));
 
+// Middleware der giver hver route adgang til currentPath, som bruges til at se hvilken url man er på
+app.use((req, res, next) => {
+	res.locals.currentPath = req.path;
+	next();
+});
+
 //Det her er basically routing som vi har gjort i
 app.get("/", (req, res) => {
-  res.render("login", {
-    title: "Hjem",
-    message: "Velkommen homie gratt gratt!",
-  });
+	res.render("login", {
+		title: "Hjem",
+		message: "Velkommen homie gratt gratt!",
+		seperator: "Før billeder",
+	});
 });
 
 app.get("/dashboard", (req, res) => {
-  res.render("dashboard", {
-    title: "Dashboard",
-    message: "Velkommen homie gratt gratt!",
-  });
+	res.render("dashboard", {
+		title: "Dashboard",
+		message: "Velkommen homie gratt gratt!",
+	});
 });
 
 app.get("/new-cleaning", (req, res) => {
-  res.render("newCleaning", {
-    title: "Ny rengøring",
-    message: "Velkommen homie gratt gratt!",
-  });
+	res.render("newCleaning", {
+		title: "Ny rengøring",
+		message: "Velkommen homie gratt gratt!",
+	});
+});
+
+app.get("/users", (req, res) => {
+	res.render("users", {
+		title: "Tilføj bruger",
+		message: "Velkommen homie gratt gratt!",
+	});
 });
 
 app.get("/add-user", (req, res) => {
-  res.render("addUser", {
-    title: "Tilføj bruger",
-    message: "Velkommen homie gratt gratt!",
-  });
+	res.render("addUser", {
+		title: "Tilføj bruger",
+		message: "Velkommen homie gratt gratt!",
+	});
 });
 
 app.get("/products", (req, res) => {
-  res.render("products", {
-    title: "Produkter",
-    message: "Velkommen homie gratt gratt!",
-  });
+	res.render("products", {
+		title: "Produkter",
+		message: "Velkommen homie gratt gratt!",
+	});
 });
 
 app.get("/stations", (req, res) => {
-  res.render("stations", {
-    title: "Stationer",
-    message: "Velkommen homie gratt gratt!",
-  });
+	res.render("stations", {
+		title: "Stationer",
+		message: "Velkommen homie gratt gratt!",
+	});
 });
 
 app.get("/add-station", (req, res) => {
-  res.render("addUser", {
-    title: "Tilføj bruger",
-    message: "Velkommen homie gratt gratt!",
-  });
+	res.render("addUser", {
+		title: "Tilføj bruger",
+		message: "Velkommen homie gratt gratt!",
+	});
 });
 
 app.listen(PORT);
