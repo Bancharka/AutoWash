49bff68847f7fee2408b9ce2ad6eec04592bc9bd
Bancharka <olipet101@gmail.com>
2025-11-19 09:59:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,6 @@ app.get("/users", async (req, res) => {
 
 	res.render("users", {
 		title: "Personale",
-		dashtitle: "Gitte Nielsen",
 		users: users,
 	});
 });
@@ -132,13 +131,13 @@ Handlebars.registerHelper("buttonVariant", function (variant) {
 app.listen(PORT);
 
 //Her er en test funktion for at se om der er hul igennem til databasen. Den trækker alt fra en tabel, her er det Users, og laver det om til en json string, null her betyder at den skal vise alt som det er, og 2 er den indentation den skal bruge.! :D
-async function testDatabase() {
-	try {
-		const results = await db.Users.findAll();
-		console.log("Brugere fra databasen:", JSON.stringify(results, null, 2));
-	} catch (error) {
-		console.error("Database fejl:", error);
-	}
-}
+// async function testDatabase() {
+// 	try {
+// 		const results = await db.Users.findAll();
+// 		console.log("Brugere fra databasen:", JSON.stringify(results, null, 2));
+// 	} catch (error) {
+// 		console.error("Database fejl:", error);
+// 	}
+// }
 
-testDatabase();
+// testDatabase();
