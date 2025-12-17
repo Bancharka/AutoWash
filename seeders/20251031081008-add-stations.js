03a7e774c2b1e65408c234f540d97292d1e2c444
Egil W <egil.wendsjo@gmail.com>
2025-12-17 21:49:59 +0100

    update seeders again

--- seeders/20251031081008-add-stations.js
+++ seeders/20251031081008-add-stations.js
@@ -2,118 +2,118 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		/**
-		 * Add seed commands here.
-		 *
-		 * Example:
-		 * await queryInterface.bulkInsert('People', [{
-		 *   name: 'John Doe',
-		 *   isBetaMember: false
-		 * }], {});
-		 */
-		await queryInterface.bulkInsert("stations", [
-			{
-				id: 1,
-				address: "Hovedgaden 12",
-				postalCode: "2750",
-				city: "Ballerup",
-				companyId: 1,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 2,
-				address: "Søndergade 44",
-				postalCode: "8700",
-				city: "Horsens",
-				companyId: 2,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 3,
-				address: "Nørrevænget 9",
-				postalCode: "7100",
-				city: "Vejle",
-				companyId: 3,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 4,
-				address: "Strandvejen 201",
-				postalCode: "2100",
-				city: "København Ø",
-				companyId: 4,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 5,
-				address: "Vesterbro 31",
-				postalCode: "5000",
-				city: "Odense C",
-				companyId: 5,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 6,
-				address: "Stationsvej 2",
-				postalCode: "4000",
-				city: "Roskilde",
-				companyId: 1,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 7,
-				address: "Vestergade 89",
-				postalCode: "8000",
-				city: "Aarhus C",
-				companyId: 2,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 8,
-				address: "Jyllingevej 178",
-				postalCode: "2720",
-				city: "Vanløse",
-				companyId: 3,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 9,
-				address: "Algade 23",
-				postalCode: "4000",
-				city: "Roskilde",
-				companyId: 4,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 10,
-				address: "Torvet 6",
-				postalCode: "6400",
-				city: "Sønderborg",
-				companyId: 5,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-		]);
-	},
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Stations", [
+      {
+        id: 1,
+        address: "Hovedgaden 12",
+        postalCode: "2750",
+        city: "Ballerup",
+        companyId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        address: "Søndergade 44",
+        postalCode: "8700",
+        city: "Horsens",
+        companyId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 3,
+        address: "Nørrevænget 9",
+        postalCode: "7100",
+        city: "Vejle",
+        companyId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 4,
+        address: "Strandvejen 201",
+        postalCode: "2100",
+        city: "København Ø",
+        companyId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 5,
+        address: "Vesterbro 31",
+        postalCode: "5000",
+        city: "Odense C",
+        companyId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 6,
+        address: "Stationsvej 2",
+        postalCode: "4000",
+        city: "Roskilde",
+        companyId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 7,
+        address: "Vestergade 89",
+        postalCode: "8000",
+        city: "Aarhus C",
+        companyId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 8,
+        address: "Jyllingevej 178",
+        postalCode: "2720",
+        city: "Vanløse",
+        companyId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 9,
+        address: "Algade 23",
+        postalCode: "4000",
+        city: "Roskilde",
+        companyId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 10,
+        address: "Torvet 6",
+        postalCode: "6400",
+        city: "Sønderborg",
+        companyId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
 
-	async down(queryInterface, Sequelize) {
-		/**
-		 * Add commands to revert seed here.
-		 *
-		 * Example:
-		 * await queryInterface.bulkDelete('People', null, {});
-		 */
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
 
-		await queryInterface.bulkDelete("stations", null, {});
-	},
+    await queryInterface.bulkDelete("stations", null, {});
+  },
 };
