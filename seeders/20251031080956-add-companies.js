064eec5fb9f5de42d6170528a8c40141454a50e0
Egil W <egil.wendsjo@gmail.com>
2025-12-17 21:45:26 +0100

    update seeder

--- seeders/20251031080956-add-companies.js
+++ seeders/20251031080956-add-companies.js
@@ -2,32 +2,32 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		return queryInterface.bulkInsert("companies", [
-			{
-				id: 1,
-				name: "Shell",
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{ id: 2, name: "OK", createdAt: new Date(), updatedAt: new Date() },
-			{
-				id: 3,
-				name: "CircleK",
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				id: 4,
-				name: "INGO",
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{ id: 5, name: "Q8", createdAt: new Date(), updatedAt: new Date() },
-		]);
-	},
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Companies", [
+      {
+        id: 1,
+        name: "Shell",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      { id: 2, name: "OK", createdAt: new Date(), updatedAt: new Date() },
+      {
+        id: 3,
+        name: "CircleK",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 4,
+        name: "INGO",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      { id: 5, name: "Q8", createdAt: new Date(), updatedAt: new Date() },
+    ]);
+  },
 
-	async down(queryInterface, Sequelize) {
-		return queryInterface.bulkDelete("companies", null, {});
-	},
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("companies", null, {});
+  },
 };
