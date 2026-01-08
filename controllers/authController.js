56350dd30fd2cc37964abb4a6a0102f8c5f6976f
Clara la Cour <denlacour@gmail.com>
2026-01-08 15:00:32 +0100

    clara forbedringer

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 
 const bcrypt = require("bcrypt");
 const db = require("../models");
+const { raw } = require("mysql2");
 
 exports.getLogin = (req, res) => {
   res.render("login", {
     title: "Log ind",
     showgraphic: true,
     hideHeader: true,
+    error: req.session.error //fejlbesked sendes til viewet
   });
+  delete req.session.error; //sletning af fejlbesked, så den ikke vises i den næste session efter fejl
 };
 
 exports.getRegister = (req, res) => {
@@ -16,19 +19,62 @@ exports.getRegister = (req, res) => {
     title: "Opret bruger",
     showgraphic: true,
     hideHeader: true,
+    error: req.session.error || "Test fejlbesked til styling" //fejlbesked sendes til viewet
   });
+  delete req.session.error; //sletning af fejlbesked, så den ikke vises i den næste session efter fejl
 };
 
 exports.postRegister = async (req, res) => {
-  const saltRounds = parseInt(process.env.SALT_ROUNDS);
 
   try {
+
+    //clara forbedringer = validation in the nation
+    const { firstName, lastName, email, password, confirmPassword } = req.body; //en mere læsbar måde at skrive koden på, gør det lidt nemmere at se sig ud af valideringen
+
+    if (!firstName || !lastName || !email || !password || !confirmPassword) {
+      req.session.error = "Alle felter skal udfyldes";
+      return res.redirect("/register");
+    }
+
+    //her køres en regEx(regular expression) til at teste om firstname og lastname indeholder tal (d = digits, altså tal) 
+    //test retunerer true hvis det er sandt og false hvis ikke
+    if (/\d/.test(firstName) || /\d/.test(lastName)) {
+      req.session.error = "Navn kan ikke indeholde tal";
+      return res.redirect("/register");
+    }
+
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      req.session.error = "Ugyldig email adresse";
+      return res.redirect("/register");
+    }
+
+    if (password !== confirmPassword) {
+      req.session.error = "Kodeord matcher ikke";
+      return res.redirect("/register");
+    }
+
+    if (password.length < 8) {
+      req.session.error = "Kodeord skal være længere end 8 tegn";
+      return res.redirect("/register");
+    }
+
+    const existingUser = await db.Users.findOne({
+      where: { email },
+      raw: true,
+    });
+
+    if (existingUser) {
+      req.session.error = "En bruger med denne email eksisterer allerede";
+      return res.redirect("/register");
+    }
+
+    const saltRounds = parseInt(process.env.SALT_ROUNDS);
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
     await db.Users.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
+      firstName,
+      lastName,
+      email,
       password: hashedPassword,
       isAdmin: false,
     });
